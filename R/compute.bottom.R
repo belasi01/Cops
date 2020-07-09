5df982d56eb597410af5df54c6f143165f066f30
Simon Bélanger <simon_belanger@uqar.ca>
2020-07-09 21:04:48 +0000

    Fix a bug when the bottom reflectance is NULL

--- R/compute.bottom.R
+++ R/compute.bottom.R
@@ -14,12 +14,14 @@ compute.bottom <- function(cops, distance.above.bottom.file.cut=0.15) {
     R.Z.Luz <- cops$LuZ.fitted * pi / cops$EdZ.fitted
     Rb.LuZ <- R.Z.Luz[ix.depth.over.bottom,]
     Rb.EuZ <- NULL
+    if (all(is.na(Rb.LuZ))) Rb.LuZ <- NULL
   }
   if("EuZ" %in% instruments.optics & !("LuZ" %in% instruments.optics)) {
     Rb.Q <- NULL
     Rb.LuZ <- NULL
     R.Z.Euz <- cops$EuZ.fitted / cops$EdZ.fitted
     Rb.EuZ <- R.Z.Euz[ix.depth.over.bottom,]
+    if (all(is.na(Rb.EuZ))) Rb.EuZ <- NULL
   }
 
   if("EuZ" %in% instruments.optics & "LuZ" %in% instruments.optics) {
@@ -27,9 +29,22 @@ compute.bottom <- function(cops, distance.above.bottom.file.cut=0.15) {
     Rb.Q <- Q[ix.depth.over.bottom,]
     R.Z.Luz <- cops$LuZ.fitted * pi / cops$EdZ.fitted
     Rb.LuZ <- R.Z.Luz[ix.depth.over.bottom,]
+    if (all(is.na(Rb.LuZ))) Rb.LuZ <- NULL
     R.Z.Euz <- cops$EuZ.fitted / cops$EdZ.fitted
     Rb.EuZ <- R.Z.Euz[ix.depth.over.bottom,]
+    if (all(is.na(Rb.EuZ))) Rb.EuZ <- NULL
   }
+
+  if (is.null(Rb.LuZ) & is.null(Rb.EuZ)) {
+    mymessage("WARNING: Bottom reflectance in NULL", head = "*")
+    return(list(bottom.depth = bottom.depth,
+                Rb.depth.over.bottom = depth.over.bottom,
+                Rb.LuZ = Rb.LuZ,
+                Rb.EuZ = Rb.EuZ,
+                Rb.Q = Rb.Q))
+
+  }
+
 
   # PLOT
   aop.cols <- rainbow.modified(length(cops$Ed0.waves))
@@ -42,18 +57,20 @@ compute.bottom <- function(cops, distance.above.bottom.file.cut=0.15) {
             xlab = expression(R[Lu]*z* ~"(unitless)"),
             col = aop.cols)
     grid(col = 1)
-    points(Rb.LuZ, rep(cops$depth.fitted[ix.depth.over.bottom], length(Rb.LuZ)),
+    if (!all(is.na(Rb.LuZ))) points(Rb.LuZ, rep(cops$depth.fitted[ix.depth.over.bottom], length(Rb.LuZ)),
            pch=19, cex=1.5, col = aop.cols)
     par(xpd = TRUE)
     legend(10^par("usr")[1], par("usr")[4], legend = cops$Ed0.waves, xjust = 0, yjust = 0, lty = 1, lwd = 2, col = aop.cols, ncol = ceiling(length(cops$Ed0.waves) / 2), cex = 0.75)
     par(xpd = FALSE)
 
+    if (!all(is.na(Rb.LuZ))){
     if(INTERACTIVE) x11(width = win.width, height = win.height)
-    plot(cops$Ed0.waves, Rb.LuZ, type="l", lwd=3,
-         ylim=c(0, min(c(0.5, max(Rb.LuZ,na.rm=T)))  ),
-         main=paste("Bottom depth:", as.character(signif(bottom.depth,3)), "m"),
-         xlab="Wavelength (nm)", ylab="Reflectance (unitless)", cex=1.5)
-    legend("topleft", expression(pi*L[u]/E[d]), lwd=3, col=1, cex=1.8)
+      plot(cops$Ed0.waves, Rb.LuZ, type="l", lwd=3,
+           ylim=c(0, min(c(0.5, max(Rb.LuZ,na.rm=T)))  ),
+           main=paste("Bottom depth:", as.character(signif(bottom.depth,3)), "m"),
+           xlab="Wavelength (nm)", ylab="Reflectance (unitless)", cex=1.5)
+      legend("topleft", expression(pi*L[u]/E[d]), lwd=3, col=1, cex=1.8)
+    }
   }
 
   if("EuZ" %in% instruments.optics & !("LuZ" %in% instruments.optics)) {
@@ -65,18 +82,20 @@ compute.bottom <- function(cops, distance.above.bottom.file.cut=0.15) {
             xlab = expression(R[Eu]*z* ~"(unitless)"),
             col = aop.cols)
     grid(col = 1)
-    points(Rb.EuZ, rep(cops$depth.fitted[ix.depth.over.bottom], length(Rb.EuZ)),
+    if (!all(is.na(Rb.EuZ))) points(Rb.EuZ, rep(cops$depth.fitted[ix.depth.over.bottom], length(Rb.EuZ)),
            pch=19, cex=1.5, col = aop.cols)
     par(xpd = TRUE)
     legend(10^par("usr")[1], par("usr")[4], legend = cops$Ed0.waves, xjust = 0, yjust = 0, lty = 1, lwd = 2, col = aop.cols, ncol = ceiling(length(cops$Ed0.waves) / 2), cex = 0.75)
     par(xpd = FALSE)
 
-    if(INTERACTIVE) x11(width = win.width, height = win.height)
-    plot(cops$Ed0.waves, Rb.EuZ, type="l", lwd=3,
-         ylim=c(0, min(c(0.5, max(Rb.EuZ,na.rm=T)))  ),
-         main=paste("Bottom depth:", as.character(signif(bottom.depth,3)), "m"),
-         xlab="Wavelength (nm)", ylab="Reflectance (unitless)", cex=1.5)
-    legend("topleft", expression(E[u]/E[d]), lwd=3, col=1, cex=1.8)
+    if (!all(is.na(Rb.EuZ))){
+      if(INTERACTIVE) x11(width = win.width, height = win.height)
+      plot(cops$Ed0.waves, Rb.EuZ, type="l", lwd=3,
+           ylim=c(0, min(c(0.5, max(Rb.EuZ,na.rm=T)))  ),
+           main=paste("Bottom depth:", as.character(signif(bottom.depth,3)), "m"),
+           xlab="Wavelength (nm)", ylab="Reflectance (unitless)", cex=1.5)
+      legend("topleft", expression(E[u]/E[d]), lwd=3, col=1, cex=1.8)
+    }
   }
 
 
@@ -93,27 +112,32 @@ compute.bottom <- function(cops, distance.above.bottom.file.cut=0.15) {
             add = TRUE,
             col = aop.cols)
     grid(col = 1)
-    points(Rb.EuZ, rep(cops$depth.fitted[ix.depth.over.bottom], length(Rb.EuZ)),
+    if (!all(is.na(Rb.EuZ))) points(Rb.EuZ, rep(cops$depth.fitted[ix.depth.over.bottom], length(Rb.EuZ)),
            pch=19, cex=1.2, col = aop.cols)
-    points(Rb.LuZ, rep(cops$depth.fitted[ix.depth.over.bottom], length(Rb.LuZ)),
+    if (!all(is.na(Rb.LuZ))) points(Rb.LuZ, rep(cops$depth.fitted[ix.depth.over.bottom], length(Rb.LuZ)),
            pch=1, cex=1.5, col = aop.cols)
     par(xpd = TRUE)
     legend(10^par("usr")[1], par("usr")[4], legend = cops$Ed0.waves, xjust = 0, yjust = 0, lty = 1, lwd = 2, col = aop.cols, ncol = ceiling(length(cops$Ed0.waves) / 2), cex = 0.75)
     par(xpd = FALSE)
 
-    if(INTERACTIVE) x11(width = win.width, height = win.height)
-    plot(cops$Ed0.waves, Rb.EuZ, type="l", lwd=3,
-         ylim=c(0, min(c(0.5, max(Rb.EuZ,na.rm=T)))  ),
-         main=paste("Bottom depth:", as.character(signif(bottom.depth,3)), "m"),
-         xlab="Wavelength (nm)", ylab="Reflectance (unitless)", cex=1.5)
-    lines(cops$Ed0.waves, Rb.LuZ, lwd=3, col=2)
-    legend("topleft", c(expression(E[u]/E[d]) , expression(pi*L[u]/E[d])), lwd=c(3,3), col=c(1,2), cex=1.8)
+    if (!all(is.na(Rb.EuZ))) {
+      if(INTERACTIVE) x11(width = win.width, height = win.height)
+      plot(cops$Ed0.waves, Rb.EuZ, type="l", lwd=3,
+           ylim=c(0, min(c(0.5, max(Rb.EuZ,na.rm=T)))  ),
+           main=paste("Bottom depth:", as.character(signif(bottom.depth,3)), "m"),
+           xlab="Wavelength (nm)", ylab="Reflectance (unitless)", cex=1.5)
+      lines(cops$Ed0.waves, Rb.LuZ, lwd=3, col=2)
+      legend("topleft", c(expression(E[u]/E[d]) , expression(pi*L[u]/E[d])), lwd=c(3,3), col=c(1,2), cex=1.8)
+    }
 
-    if(INTERACTIVE) x11(width = win.width, height = win.height)
-    plot(cops$Ed0.waves, Rb.Q, type="l", lwd=3,
-         #ylim=c(0, min(c(0.5, max(Rb.EuZ,na.rm=T)))  ),
-         main="Bottom BRDF",
-         xlab="Wavelength (nm)", ylab=expression("Q-factor (",sr^-1,")"), cex=1.5)
+    if (!all(is.na(Rb.Q))) {
+      if(INTERACTIVE) x11(width = win.width, height = win.height)
+      plot(cops$Ed0.waves, Rb.Q, type="l", lwd=3,
+           #ylim=c(0, min(c(0.5, max(Rb.EuZ,na.rm=T)))  ),
+           main="Bottom BRDF",
+           xlab="Wavelength (nm)", ylab=expression("Q-factor (",sr^-1,")"), cex=1.5)
+    }
+
   }
 
   return(list(bottom.depth = bottom.depth,
